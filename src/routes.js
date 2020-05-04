7197309ce7153ff7a7223a795fe2cf98a56cfff7
Mateus S. Oliveira <devmateusoliveira@gmail.com>
2020-05-04 01:15:14 -0300

    feat(setup): setup ingredients page

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import  { BrowserRouter, Route } from 'react-router-dom';
 
 import PizzaSize from './pages/PizzaSize';
 import PizzaCrust from './pages/PizzaCrust';
+import PizzaIngredients from './pages/PizzaCrust';
 
 export default function Routes(){
     return (
           <BrowserRouter>
             <Route path="/" component={PizzaSize} />
             <Route path="/crust" component={PizzaCrust}/>
+            <Route path="/ingredients" component={PizzaIngredients}/>
            </BrowserRouter>
     );
 }
